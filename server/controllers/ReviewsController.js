fc8170e770e90df00f92f440ca7333038eea3f6e
yyankova <yana.o.yankova@gmail.com>
2014-10-09 13:08:15 +0300

    Update Review schema to hold ObjectIds to User and Product instead of string. Full CRUD in the Review controller.

--- server/controllers/ReviewsController.js
+++ server/controllers/ReviewsController.js
@@ -2,8 +2,10 @@ var Review = require('mongoose').model('Review');
 
 module.exports = {
     createReview: function(req, res, next) {
+        //req.body = {text: '', productId: '', product: 'name'}
         var newReviewData = req.body;
         newReviewData.date = new Date();
+        newReviewData.userId = req.user._id;
         newReviewData.user = req.user.username;
         Review.create(newReviewData, function(err, review) {
             if (err) {
@@ -15,14 +17,43 @@ module.exports = {
         });
     },
     getAllReviewsForProduct: function(req, res) {
-        // /api/products/reviews
+        // /api/products/:id/reviews
         var productId = req.body.productId;
-        Purchase.find({product : productId}).exec(function(err, collection) {
+        Purchase.find({productId : productId}).exec(function(err, collection) {
             if (err) {
                 console.log('Error while listing reviews: ' + err);
             }
 
             res.send(collection);
-        })
+        });
+    },
+    getAllReviewsFromUser: function(req, res) {
+        // /api/users/:id/reviews
+        var userId = req.body.productId;
+        Purchase.find({userId : userId}).exec(function(err, collection) {
+            if (err) {
+                console.log('Error while listing reviews: ' + err);
+            }
+
+            res.send(collection);
+        });
+    },
+    remove: function(req, res){
+        // delete on /api/users/:userId/reviews/:reviewId
+        var id = req.params['reviewId'];
+        Purchase.findOneAndRemove({_id: id}).exec(function(err){
+            if(err){
+                console.log('Error deleting review');
+            }
+        });
+    },
+    update: function(req, res){
+        // put on /api/users/:userId/reviews/:reviewId
+        var id = req.params['reviewId'];
+        Purchase.findOneAndUpdate({_id: id}, req.body).exec(function(err){
+            if(err){
+                console.log('Error deleting review');
+            }
+        });
     }
 }
