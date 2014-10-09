e761599468df8c491ce6f0b7a49e0928b7e743da
idoychinov <doychinov.ivan@gmail.com>
2014-10-10 00:05:52 +0300

    Changes to users and products layout. Check for proper roll to show create product button

--- app/js/controllers/productsListCtrl.js
+++ app/js/controllers/productsListCtrl.js
@@ -1,6 +1,6 @@
-app.controller('ProductsListCtrl', function ($scope, $location, ProductsData) {
+app.controller('ProductsListCtrl', function ($scope, $location, ProductsData, identity) {
     $scope.products = ProductsData.ProductsData.query();
-
+    $scope.isAdmin = identity.isAuthorizedForRole('admin');
 
     $scope.createProductRedirect = function () {
         $location.path('/products/create');
