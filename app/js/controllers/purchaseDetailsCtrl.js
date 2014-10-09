152f05f972f8d43550fd783d418282af82205f61
yyankova <yana.o.yankova@gmail.com>
2014-10-09 21:47:02 +0300

    Table for product details

--- app/js/controllers/purchaseDetailsCtrl.js
+++ app/js/controllers/purchaseDetailsCtrl.js
@@ -1,10 +1,16 @@
 'use strict';
 
 app.controller('PurchaseDetailsCtrl', function PurchaseDetailsCtrl($scope, $routeParams, notifier, purchasesData) {
+    $scope.leaveReview = leaveReview;
+
     purchasesData.getPurchaseDetails($routeParams.id)
         .then(function(data){
-            $scope.purchase = data;
+            $scope.currPurchase = data;
         }, function(err){
             notifier.error(err.message);
         });
+
+    function leaveReview(){
+
+    }
 });
