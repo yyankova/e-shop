dcb251524ed18b8818772da56096ddb5612985d4
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-10-10 04:33:54 +0300

    fixed reviews and added functionality for listing all reviews

--- app/js/controllers/productDetailsCtrl.js
+++ app/js/controllers/productDetailsCtrl.js
@@ -1,15 +1,20 @@
-app.controller('ProductDetailsCtrl', function($scope, $routeParams, cachedProducts) {
-    $scope.product = cachedProducts.query().$promise.then(function(collection) {
-        collection.forEach(function(product) {
+app.controller('ProductDetailsCtrl', function ($scope, $routeParams, cachedProducts, reviewData) {
+    $scope.product = cachedProducts.query().$promise.then(function (collection) {
+        collection.forEach(function (product) {
             if (product._id === $routeParams.id) {
                 $scope.product = product;
             }
         })
     });
 
+    $scope.reviews = reviewData.getReviewsByProductId($routeParams.id)
+        .then(function (success) {
+            $scope.reviews = success;
+        });
+
     $scope.buy = buy;
 
-    function buy(productId){
+    function buy(productId) {
         console.log(productId);
     };
 });
